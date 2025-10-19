0575eaa957bd989492c7115ba5d3c1c7ce97f47a
Marwin Härttrich <39224983+haerttrich@users.noreply.github.com>
2025-10-19 21:32:53 +0200

    rm relay import from custom_screen

--- boards/shields/nice_custom/custom_screen.c
+++ boards/shields/nice_custom/custom_screen.c
@@ -3,10 +3,6 @@
 #include <zmk/display.h>
 #include "display/screens/screen_draw.h"
 
-#if IS_ENABLED(CONFIG_ZMK_SPLIT)
-#include "display/relay/widget_relay.h"
-#endif
-
 lv_obj_t *zmk_display_status_screen() {
     return screen_display();
 }
